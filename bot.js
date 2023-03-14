2f7c5cacc469a43d121e18147ab05a26b46ac531
Andrea055 <vivanco11.ac@gmail.com>
2023-03-14 15:46:30 +0000

    rework command

--- bot.js
+++ bot.js
@@ -1,21 +1,26 @@
 import TelegramBot from 'node-telegram-bot-api';
 import { upload } from './commands.js';
-import { generate_file_json, gen_json, token } from './helper.js';
+import { generate_file_json, gen_json, token, commit_and_push } from './helper.js';
 import { exec } from 'child_process';
 
 async function main() {
 
-    exec("git clone https://github.com/bananadroid-devices/bananadroid_devices.git", (error, stdout, stderr) => {
-        if (error) {
-            console.log(`error: ${error.message}`);
-            return;
-        }
-        if (stderr) {
-            console.log(`stderr: ${stderr}`);
-            return;
-        }
-        console.log(`stdout: ${stdout}`);
-    });
+    try {
+        exec("git clone https://github.com/bananadroid-devices/bananadroid_devices.git", (error, stdout, stderr) => {
+            if (error) {
+                console.log(`error: ${error.message}`);
+                return;
+            }
+            if (stderr) {
+                console.log(`stderr: ${stderr}`);
+                return;
+            }
+            console.log(`stdout: ${stdout}`);
+        });
+    } catch (error) {
+        console.log(error)
+    }
+
 
 
     // Create a bot that uses 'polling' to fetch new updates
@@ -27,20 +32,21 @@ async function main() {
         const device_name = args[0];
         const download_url = args[1];
         const changelog_device = args[2];
-        const changelog_source = args[3];
-
+        const photo = args[3];
         if(device_name == null || download_url == null){
             bot.sendMessage(chatId, "Missing arguments");
-            bot.sendMessage(chatId, "Usage: /upload device_name download_url changelog_source(optional) changelog_device(optional)");
+            bot.sendMessage(chatId, "Usage: /upload device_name download_url changelog_device(optional) phone_image_url");
         }else {
-            const json = gen_json(device_name, download_url, changelog_source, changelog_device);
-            generate_file_json(json, device_name);
-            const commit = commit_and_push();
-            if(commit){
+            try {
+                const json = gen_json(device_name, download_url, changelog_device, photo);
+                generate_file_json(json, device_name);
+                const commit = commit_and_push(device_name);
                 bot.sendMessage(chatId, "Device updated successfully");
-            }else {
-                bot.sendMessage(chatId, "Error during updating device");
+            } catch (error) {
+                console.error(error)
+                bot.sendMessage(chatId, "Error during uploading update to git")
             }
+
         }
     });
 
@@ -50,7 +56,7 @@ async function main() {
     bot.on('message', (msg, match) => {
         const chatId = msg.chat.id;
         if(msg.text == "/upload")
-            bot.sendMessage(chatId, "Usage: /upload device_name download_url changelog_source(optional) changelog_device(optional)");
+            bot.sendMessage(chatId, "Usage: /upload device_name download_url changelog_device(optional) phone_image_url");
     });
 }
 
--- helper.js
+++ helper.js
@@ -7,20 +7,14 @@ export function token() {
     return parsed.token;
 }
 
-/*
-{
-    "file": filename,
-    "download_url": url,
-    "changelog_source": changelog_url,
-    "changelog_device": changelog_device    
-*/
-
-export function gen_json(filename, download, changelog_source, changelog_device) {
+export function gen_json(filename, download, changelog_device, photo) {
+    let version = download.split("/")[download.split("/").length - 2].split(".")[0];
     return {
         file: filename,
         download_url: download,
-        "changelog_source": changelog_source,
-        "changelog_device": changelog_device
+        "changelog_device": changelog_device,
+        "version": version,
+        "photo": photo
     }
 }
 
@@ -28,7 +22,7 @@ export function generate_file_json(json, device_name){
     fs.writeFileSync("./bananadroid_devices/" + device_name + ".json", JSON.stringify(json));
 }
 
-export function commit_and_push() {
+export function commit_and_push(device) {
     exec(`cd bananadroid_devices && git pull && git add . && git commit -m "update ${device}" && git push`, (error, stdout, stderr) => {
         if (error) {
             console.log(error);
